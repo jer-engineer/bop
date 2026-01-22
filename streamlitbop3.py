e6f1a8cf84a980e2a8eaa011b471a0bd65876ecd
jer-engineer <jeremias.a.maier@gmail.com>
2026-01-22 10:15:45 +0200

    Update streamlitbop3.py

--- streamlitbop3.py
+++ streamlitbop3.py
@@ -13,8 +13,10 @@ st.set_page_config(page_title="Cloud Weld Manager Pro", layout="wide", page_icon
 DEFAULT_LINE_COL = "LINE No"
 DEFAULT_WELD_COL = "Weld No"
 DEFAULT_REF_COLS = ["TYPE 1", "TYPE 2", "SYSTEM"] # <--- ΤΑ ΝΕΑ DEFAULTS
-REPO_MASTER_FILE = "master.xlsx" # Το όνομα του αρχείου που θα ψάχνει αυτόματα
-
+# Βρίσκουμε τον φάκελο που βρίσκεται το αρχείο .py
+current_dir = os.path.dirname(os.path.abspath(__file__))
+# Φτιάχνουμε την πλήρη διαδρομή για το master.xlsx
+REPO_MASTER_FILE = os.path.join(current_dir, "master.xlsx")
 # --- 0. ΛΕΙΤΟΥΡΓΙΕΣ ΑΠΟΘΗΚΕΥΣΗΣ (PERSISTENCE) ---
 SETTINGS_FILE = "settings.json"
 
